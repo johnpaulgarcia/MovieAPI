46ea70872a234db11cf1de464b0c6275629799a2
gjohnson186 <49846370+gjohnson186@users.noreply.github.com>
2019-06-05 17:59:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function ntime(){
     let timeElem = document.getElementById('time');
     let date = new Date();
     let hour = date.getHours()>12?date.getHours()-12:date.getHours();
-    let min = (date.getMinutes()-9)<10?Number(date.getMinutes())-9:date.getMinutes()-9;
+    let min = (date.getMinutes()-9)<10?(date.getMinutes()-9):date.getMinutes()-9;
     let sec = date.getSeconds()<10?`0${date.getSeconds()}`:date.getSeconds();
     let time = `${hour}:${min}:${sec}`;
     timeElem.innerText=time;
